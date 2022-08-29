2985b04b1c74ee8c53cdb812042fe8e59b6ff37b
bemrekalkan <bemre.kalkan@gmail.com>
2022-08-29 20:35:50 +0300

    admin superuser created

--- todo/models.py
+++ todo/models.py
@@ -1,18 +1,19 @@
 from django.db import models
 
 status_choices = [
-    ('C', 'Completed')
-    ('P', 'Pending')
+    ('C', 'Completed'),
+    ('P', 'Pending'),
     ('I', 'In-Progress')
 ]
 
 priority_choices = [
-    ('1', '1️⃣')
-    ('2', '2️⃣')
-    ('3', '3️⃣')
-    ('4', '4️⃣')
-    ('5', '5️⃣')
+    ('1', '1️⃣'),
+    ('2', '2️⃣'),
+    ('3', '3️⃣'),
+    ('4', '4️⃣'),
+    ('5', '5️⃣'),
 ]
+
 class Todo(models.Model):
     title = models.CharField(max_length=70)
     description = models.TextField()
@@ -20,7 +21,7 @@ class Todo(models.Model):
     priority = models.CharField(max_length=2, choices=priority_choices)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-
+    
     def __str__(self):
         return self.title
 

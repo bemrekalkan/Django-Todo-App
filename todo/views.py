4bd42bb2885a7f765f29674a42c64332132ce71b
bemrekalkan <bemre.kalkan@gmail.com>
2022-08-29 21:22:06 +0300

    ..

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
 from .forms import TodoForm
@@ -13,6 +13,12 @@ def home(request):
 
 def todo_create(request):
     form = TodoForm()
+
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
 
     context = {
         "form" : form

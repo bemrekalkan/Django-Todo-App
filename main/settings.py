84016a42a2ece3017ddea392c520c9ff6e16e044
bemrekalkan <bemre.kalkan@gmail.com>
2022-09-03 11:49:51 +0300

    ..

--- main/settings.py
+++ main/settings.py
@@ -127,3 +127,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import django_heroku
+#? Activate Django-Heroku 👇:
+django_heroku.settings(locals())
